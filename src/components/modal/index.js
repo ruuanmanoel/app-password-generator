367ebde09227886e619360420b3ceaface9909f9
Ruan Manoel <ruuanmanoel@gmail.com>
2025-02-25 19:13:02 -0300

    estilizando modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,15 +1,76 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable, TouchableOpacity } from "react-native";
 
 export function ModalPassaword({password}){
     return(
-        <View>
-            <Text>{password}</Text>
+        <View style={styles.container}>
+          <View style={styles.content}>
+            <Text style={styles.title}>Senha Gerada</Text>
+            <Pressable style={styles.innerPassword}> 
+                <Text style={styles.text}>123123</Text>
+            </Pressable>
+            <View style={styles.buttonArea}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Voltar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.buttonSave]}>
+                    <Text style={styles.buttonSaveText}>Salvar senha</Text>
+                </TouchableOpacity>
+            </View>
+          </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-
+        backgroundColor: "rgba(24,24,24,0.6)",
+        flex: 1,
+        alignItems:'center',
+        justifyContent: 'center'
+    },
+    content:{
+        backgroundColor: '#FFF',
+        width: '85%',
+        paddingBlock: 24,
+        alignItems: 'center',
+        justifyContent:'center',
+        borderRadius: 8
+    },
+    title:{
+        fontSize:20,
+        fontWeight: 'bold',
+        color: '#000',
+        marginBottom:24
+    },
+    innerPassword:{
+        backgroundColor: '#0e0e0e',
+        width: '90%',
+        padding: 14,
+        borderRadius: 8
+    },
+    text:{
+        color: '#fff',
+        textAlign: 'center'
+    },
+    buttonArea:{
+        flexDirection: "row",
+        width: '90%',
+        marginTop: 8,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    button:{
+        flex: 1,
+        alignItems: 'center',
+        marginBlock:14,
+        padding:8
+    },
+    buttonSave:{
+        backgroundColor: '#392DE9',
+        borderRadius:8
+    },
+    buttonSaveText:{
+        color:'#fff',
+        fontWeight: 'bold'
     }
 }) 
