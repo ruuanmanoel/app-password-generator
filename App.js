1755ada5c67e355c69593ccf935172f778f23a3b
Ruan Manoel <ruuanmanoel@gmail.com>
2025-02-25 22:57:52 -0300

    navegação e rotas

--- App.js
+++ App.js
@@ -1,90 +1,12 @@
-import { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  Modal,
-} from "react-native";
-import Slider from "@react-native-community/slider";
-import { ModalPassaword } from "./src/components/modal";
+import { NavigationContainer } from "@react-navigation/native";
 
-let charset = "abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+import { View } from "react-native";
+import { Routes } from "./src/routes";
 
 export default function App() {
-  const [size, setSize] = useState(10);
-  const [passwordValue, setPassowordValue] = useState("");
-  const [modalVisible, setModalVisible] = useState(false);
-
-  function generatePassword() {
-    let password = "";
-    for (let i = 0, n = charset.length; i < size; i++) {
-      password += charset.charAt(Math.floor(Math.random() * n));
-    }
-    setPassowordValue(password);
-    setModalVisible(true);
-  }
-
   return (
-    <View style={styles.container}>
-      <Image source={require("./src/assets/logo.png")} style={styles.logo} />
-      <Text style={styles.title}>{size} Caracteres</Text>
-      <View style={styles.area}>
-        <Slider
-          style={{ height: 50 }}
-          minimumValue={6}
-          maximumValue={20}
-          value={size}
-          onValueChange={(value) => setSize(Math.floor(value))}
-        />
-      </View>
-      <TouchableOpacity style={styles.button} onPress={generatePassword}>
-        <Text style={styles.buttonText}>Gerar Senha</Text>
-      </TouchableOpacity>
-
-      <Modal visible={modalVisible} animationType="fade" transparent={true}>
-        <ModalPassaword
-          password={passwordValue}
-          handleClose={() => setModalVisible(false)}
-        />
-      </Modal>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f3f3ff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  logo: {
-    marginBottom: 60,
-  },
-  area: {
-    marginBlock: 14,
-    width: "80%",
-    backgroundColor: "#FFF",
-    borderRadius: 8,
-    padding: 8,
-  },
-  button: {
-    backgroundColor: "#392de9",
-    width: "80%",
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 8,
-    marginBottom: 18,
-  },
-  buttonText: {
-    color: "#FFF",
-    fontSize: 20,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-});
